31fa13367951380c5d6f66e690d141000798f7d4
enfibotec-cmd <enfibotec@gmail.com>
2026-09-24 11:58:01 -0500

    Update esquema.sql

--- db/esquema.sql
+++ db/esquema.sql
@@ -1,12 +1,16 @@
-CREATE DATABASE mi_app
+CREATE DATABASE IF NOT EXISTS mi_app
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 USE mi_app;
 
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO usuarios (nombre, email)
+VALUES
+    ('Usuario Demo', 'demo@ejemplo.com');
